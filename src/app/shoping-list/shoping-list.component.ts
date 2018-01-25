fe88415ebc5baa803564e9c1655f77964d255005
okaminouta <434563z@gmail.com>
2018-01-25 18:24:42 +0200

    50-?

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.module';
 
 @Component({
   selector: 'app-shoping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
-
+ingredients: Ingredient[] = [
+  new Ingredient('apples', 5),
+  new Ingredient('apples', 6),
+  new Ingredient('apples', 8)
+];
   constructor() { }
 
   ngOnInit() {
